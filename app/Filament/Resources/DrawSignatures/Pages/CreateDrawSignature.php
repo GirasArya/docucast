68b1d6cedec528667438bf08a386aaa66c9b1226
GirasArya <girasarya130@gmail.com>
2026-07-10 09:50:46 +0700

    add : added resource for draw signature feat

---
+++ app/Filament/Resources/DrawSignatures/Pages/CreateDrawSignature.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\DrawSignatures\Pages;
+
+use App\Filament\Resources\DrawSignatures\DrawSignatureResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateDrawSignature extends CreateRecord
+{
+    protected static string $resource = DrawSignatureResource::class;
+}
